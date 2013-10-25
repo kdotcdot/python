3c727bd774f18412419fb75f0f21630f80edd67b
K.C. Young <k.c.young1@gmail.com>
2013-10-25 16:57:34 -0400

    Learning in Python
    CodeAcademy content of code

---
+++ pythonLearning.py
@@ -0,0 +1,28 @@
+# my_int is set to 7 below. What do you think
+my_int = 7
+
+print my_int
+
+#span function
+def spam():
+    eggs = 10**2 #Exponentiate
+    return eggs
+print spam()
+
+""" This is some commented shit I'm writing
+This is some more shit """
+
+# Calculate a tip
+
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal + meal*tip
+
+print("%.2f" % total)
+
+# Use of escape backslash
+'Help! Help! I\'m being repressed!'
+
